8542d6160983f620a7057ce7d81860f9a06593bf
ansh23092002 <anshshrama60oo@gmail.com>
2025-06-29 15:39:32 +0530

    Adding screen shot

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -18,7 +18,7 @@ function MovieCard({ movie }) {
       <img
         src={movie.Poster !== 'N/A' ? movie.Poster : 'https://via.placeholder.com/150'}
         alt={movie.Title}
-        className="w-full h-60 object-cover"
+        className="w-full h-60 object-cover "
       />
       <h2 className="text-md font-semibold mt-2 justify-center flex">{movie.Title}</h2>
       <p className='justify-center flex'>{movie.Year}</p>
